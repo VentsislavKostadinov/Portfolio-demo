9086dba839623300b90010cb730a9d9a2db11e19
VentsislavKostadinov <ventsislav_kostadinov@abv.bg>
2020-03-24 19:52:56 +0200

    changes

--- js/nav-change-color.js
+++ js/nav-change-color.js
@@ -1,13 +1,38 @@
 $(document).ready(function(){
+
     $(window).scroll(function(){
+
         let scroll = $(window).scrollTop();
+        let viewWidth = $(window).width();
+        let btn = $('#top-button');
+
+        // Desktop navigation scroll change opacity
+        
         if (scroll > 420) {
             $(".navbar").css({'background' : '#000000', 'opacity' : '0.7'});
-
         }
-
-       else{
+       else {
            $(".navbar").css({'background' : 'transparent', 'opacity': '1'});
        }
-    })
+
+       // For Mobile: max-width 991px and scrolling 220px hide navigation and showing pop-tip button - Go to top
+
+       if(viewWidth <= 991 && scroll > 220) {
+           $('.fixed-top').css({'visibility' : 'hidden'});
+           btn.css({'display' : 'block'});
+
+       } else {
+           $('.fixed-top').css({'visibility' : 'visible'});
+           btn.css({'display' : 'none'});
+       }
+
+      btn.on('click', function (e) {
+
+          e.preventDefault();
+          $('html, body').animate({scrollTop:0}, '300');
+      })
+
+    });
+
+
 });
