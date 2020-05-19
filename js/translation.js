312ffaa6ba306e3352e108f6b141b73f34610e76
VentsislavKostadinov <ventsislav_kostadinov@abv.bg>
2020-05-19 16:05:01 +0300

    update

--- js/translation.js
+++ js/translation.js
@@ -1,23 +1,30 @@
 let langTranslate = {
     'en': {
-        'home': 'Home',
-        'about': 'About us',
-        'projects': 'Projects',
-        'particles': 'Why us',
-        'deliver': 'We deliver your stunning website',
-        'theme': 'Choose your theme',
-        'business': 'Let your business be successful'
+        'home-tr': 'Home',
+        'about-tr': 'About us',
+        'projects-tr': 'Projects',
+        'particles-tr': 'Why us',
+        'deliver-tr': 'We deliver your stunning website',
+        'theme-tr': 'Choose your theme',
+        'business-tr': 'Let your business be successful',
+        'what-we-do-tr': 'What we do',
+        'develop-tr': 'DEVELOPEMNT',
+        'qa-tr': 'TESTING',
+        'maintain-tr': 'MAINTAINING'
 
     },
     'bg': {
-        'home': 'Начало',
-        'about': 'За нас',
-        'projects': 'Проекти',
-        'particles': 'Защо нас',
-        'deliver': 'Ние доставяме вашия зашеметяващ уебсайт',
-        'theme': 'Изберете тема',
-        'business': 'Нека вашият бизнес бъде успешен'
-
+        'home-tr': 'Начало',
+        'about-tr': 'За нас',
+        'projects-tr': 'Проекти',
+        'particles-tr': 'Защо нас',
+        'deliver-tr': 'Ние доставяме вашия зашеметяващ уебсайт',
+        'theme-tr': 'Изберете тема',
+        'business-tr': 'Нека вашият бизнес бъде успешен',
+        'what-we-do-tr': 'С какво се занимаваме',
+        'develop-tr': 'РАЗРАБОТВАНЕ',
+        'qa-tr': 'ТЕСТВАНЕ',
+        'maintain-tr': 'ПОДДРЪЖКА'
     }
 };
 
@@ -25,12 +32,9 @@ let langTranslate = {
 
 $(function() {
     $('.translate').click(function(e) {
-
         e.preventDefault();
-
         let lang = $(this).attr('id');
-
-        $('.lang').each(function(index, item) {
+        $('.lang').each(function() {
             $(this).text(langTranslate[lang][$(this).attr('key')]);
         });
     });
